4ccd2767ded7bd618c16cafb44374ad4e826c1b3
Timothy Hinchcliffe <gitprojects.qm@spidererrol.co.uk>
2024-08-01 18:55:04 +0000

    Bugfix was editing only one part of colour.

--- src/components/Timer/EditColour.tsx
+++ src/components/Timer/EditColour.tsx
@@ -10,6 +10,7 @@ import { ClockIcon, DeleteIcon, SecondsIcon } from "@/components/Icons"
 
 interface EditOnePartProps {
     FromTo: string
+    EditStart: boolean
     Edit: tState
     SecondsMode: tState
     colour: TimerStageColorData
@@ -19,18 +20,26 @@ interface EditOnePartProps {
     colourno: number
 }
 
-function EditOnePart({ FromTo, Edit, SecondsMode, colour, updateTimer, timer, stageno, colourno }: EditOnePartProps) {
+function EditOnePart({ FromTo, EditStart, Edit, SecondsMode, colour, updateTimer, timer, stageno, colourno }: EditOnePartProps) {
     return <>
         <tr>
             <th onClick={() => Edit.toggle()}>{FromTo}:</th>
-            <td><ViewEditDuration edit={Edit} useSeconds={SecondsMode} duration={colour.startpoint} updateDuration={ud => { updateTimer(timer.id, t => ud(t.stages[stageno].colors[colourno].startpoint)) }} />               </td>
-            <td onClick={() => Edit.toggle()}><ColourSwatch colour={colour.startcolor} />{Edit.state
+            <td><ViewEditDuration edit={Edit} useSeconds={SecondsMode} duration={EditStart ? colour.startpoint : colour.endpoint} updateDuration={ud => {
+                updateTimer(timer.id, t => {
+                    var col = t.stages[stageno].colors[colourno]
+                    return ud(EditStart ? col.startpoint : col.endpoint)
+                })
+            }} />               </td>
+            <td onClick={() => Edit.toggle()}><ColourSwatch colour={EditStart ? colour.startcolor : colour.endcolor} />{Edit.state
                 ? <FingerButton onClick={e => { e.stopPropagation(); SecondsMode.toggle() }}>{SecondsMode.state
-                    ? <ClockIcon duration={colour.endpoint} />
-                    : <SecondsIcon duration={colour.endpoint} />}</FingerButton>
+                    ? <ClockIcon duration={EditStart ? colour.startpoint : colour.endpoint} />
+                    : <SecondsIcon duration={EditStart ? colour.startpoint : colour.endpoint} />}</FingerButton>
                 : <></>}</td>
         </tr>
-        <ViewEditColour Edit={Edit} colour={colour.startcolor} updateColour={uc => updateTimer(timer.id, t => uc(t.stages[stageno].colors[colourno].startcolor))} />
+        <ViewEditColour Edit={Edit} colour={EditStart ? colour.startcolor : colour.endcolor} updateColour={uc => updateTimer(timer.id, t => {
+            var col = t.stages[stageno].colors[colourno]
+            return uc(EditStart ? col.startcolor : col.endcolor)
+        })} />
     </>
 }
 
@@ -48,8 +57,8 @@ export default function EditColour({ timer, stageno, colourno, updateTimer }: Ed
     const colour = timer.stages[stageno].colors[colourno]
     return <div className="editcolour">
         <table className="editcolourtable">
-            <EditOnePart FromTo="From" Edit={EditFrom} SecondsMode={SecondsMode} colour={colour} updateTimer={updateTimer} timer={timer} stageno={stageno} colourno={colourno} />
-            <EditOnePart FromTo="To" Edit={EditTo} SecondsMode={SecondsMode} colour={colour} updateTimer={updateTimer} timer={timer} stageno={stageno} colourno={colourno} />
+            <EditOnePart FromTo="From" EditStart={true} Edit={EditFrom} SecondsMode={SecondsMode} colour={colour} updateTimer={updateTimer} timer={timer} stageno={stageno} colourno={colourno} />
+            <EditOnePart FromTo="To" EditStart={false} Edit={EditTo} SecondsMode={SecondsMode} colour={colour} updateTimer={updateTimer} timer={timer} stageno={stageno} colourno={colourno} />
         </table>
         <div className="controls"><FingerButton title="Delete colour change" onClick={() => updateTimer(timer.id, t => t.stages[stageno].delColour(colourno))}><DeleteIcon /></FingerButton></div>
     </div>
